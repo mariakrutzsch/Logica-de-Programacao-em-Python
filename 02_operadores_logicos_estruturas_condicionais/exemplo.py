a36c96348e4997fb449d76d39f2eb965d2e2fc8c
Maria Eduarda <maria_krutzsch@estudante.sesisenai.org.br>
2026-08-31 12:25:07 -0300

    Exemplos

--- 02_operadores_logicos_estruturas_condicionais/exemplo.py
+++ 02_operadores_logicos_estruturas_condicionais/exemplo.py
@@ -9,3 +9,42 @@ possui_carteira = True
 
 resultado = idade >= 18 and possui_carteira
 print(resultado)
+
+#or
+#Pelo menos uma condição precisa ser verdadeira.
+
+idade = 16
+acompanhado =  True
+
+resultado = idade >= 18 or acompanhado
+print(resultado)
+
+#not
+#Inverte o resultado de uma condição
+
+aluno_matriculado = True
+print(not aluno_matriculado)
+
+#Operadores de Comparação
+
+idade = 18
+
+print(idade == 18)
+print(idade != 18)
+print(idade > 18)
+print(idade < 18)
+print(idade >= 18)
+print(idade <= 18)
+
+#3. Estrutura IF
+
+if idade >= 18:
+    print("maior de idade")
+
+#4. Estrutura IF/else
+
+if idade = 16
+if idade >= 18:
+    print("maior de idade")
+else:
+    print("menor de idade")
